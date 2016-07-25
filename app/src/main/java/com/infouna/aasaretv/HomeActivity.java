1a9316eae8da300c61f2ebc27477a224178d1498
darshanjagthap <darshandjagthap@gmail.com>
2016-07-25 22:54:33 +0530

    API

--- app/src/main/java/com/infouna/aasaretv/HomeActivity.java
+++ app/src/main/java/com/infouna/aasaretv/HomeActivity.java
@@ -4,10 +4,22 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.infouna.aasaretv.app.AppConfig;
+import com.infouna.aasaretv.app.AppController;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import io.vov.vitamio.MediaPlayer;
 import io.vov.vitamio.Vitamio;
@@ -18,16 +30,21 @@ import io.vov.vitamio.widget.VideoView;
  */
 public class HomeActivity extends Activity {
 
-    private String path;
+    public String path;
     private VideoView mVideoView;
     private Activity myActivity;
     private static ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        makeJsonObjectRequest(); // calling here to ensure that URL is recieved early
+
         Vitamio.isInitialized(getApplicationContext());
         setContentView(R.layout.activity_home);
+
         LinearLayout scl = (LinearLayout) findViewById(R.id.sc); //links to webview activity
         LinearLayout vdl = (LinearLayout) findViewById(R.id.vd);
         LinearLayout dol = (LinearLayout) findViewById(R.id.donate);
@@ -35,19 +52,27 @@ public class HomeActivity extends Activity {
         LinearLayout share = (LinearLayout) findViewById(R.id.share);
         this.myActivity = this;
         mVideoView = (VideoView) findViewById(R.id.vitamio_videoView);
-        path = "rtmp://103.230.222.250:1935/livestream/livestream/livestream";
-        mVideoView.setVideoPath(path);
-        mVideoView.requestFocus();
-        mVideoView.setOnErrorListener(mOnErrorListener);
-        progressDialog = ProgressDialog.show(this, "", "Streaming...Touch video for fullscreen", true);
+
+        new Handler().postDelayed(new Runnable() {  //adding a delay to ensure the path variable has been set
+
+            @Override
+            public void run() {
+
+                mVideoView.setVideoPath(path);
+                mVideoView.requestFocus();
+                mVideoView.setOnErrorListener(mOnErrorListener);
+                progressDialog = ProgressDialog.show(HomeActivity.this, "", "Streaming...Touch video for fullscreen", true);
+
+            }
+        }, 5000);
 
         mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-           @Override
-           public void onPrepared(MediaPlayer mediaPlayer) {
-               progressDialog.dismiss();
-               mVideoView.start();
-               mediaPlayer.setPlaybackSpeed(1.0f);
-          }
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                progressDialog.dismiss();
+                mVideoView.start();
+                mediaPlayer.setPlaybackSpeed(1.0f);
+            }
         });
 
         mVideoView.setOnTouchListener(new View.OnTouchListener() {
@@ -71,7 +96,7 @@ public class HomeActivity extends Activity {
         vdl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i =new Intent(myActivity.getApplicationContext(), WebViewVideos.class);
+                Intent i = new Intent(myActivity.getApplicationContext(), WebViewVideos.class);
                 startActivity(i);
             }
         });
@@ -79,7 +104,7 @@ public class HomeActivity extends Activity {
         dol.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i =new Intent(myActivity.getApplicationContext(),WebViewDonate.class);
+                Intent i = new Intent(myActivity.getApplicationContext(), WebViewDonate.class);
                 startActivity(i);
             }
         });
@@ -87,7 +112,7 @@ public class HomeActivity extends Activity {
         conl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(myActivity.getApplicationContext(),WebViewContact.class);
+                Intent i = new Intent(myActivity.getApplicationContext(), WebViewContact.class);
                 startActivity(i);
             }
         });
@@ -104,19 +129,55 @@ public class HomeActivity extends Activity {
         });
 
     }
+
     private MediaPlayer.OnErrorListener mOnErrorListener = new MediaPlayer.OnErrorListener() {
         @Override
         public boolean onError(MediaPlayer mp, int what, int extra) {
-            Toast.makeText(getApplicationContext(),"Sorry, Channel not broadcasting",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Sorry, Channel not broadcasting", Toast.LENGTH_SHORT).show();
             progressDialog.dismiss();
             return true;
         }
     };
 
     @Override
-    public void onBackPressed()
-    {
+    public void onBackPressed() {
         myActivity.finish();
+    }
+
+    private void makeJsonObjectRequest() {
+
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                AppConfig.URL_STREAM, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+
+                try {
+                    // Parsing json object response
+                    // response will be a json object
+                    path = response.getString("result");
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
     }
 
 }
---
+++ app/src/main/java/com/infouna/aasaretv/app/AppConfig.java
@@ -0,0 +1,7 @@
+package com.infouna.aasaretv.app;
+
+public class AppConfig {
+
+    public static String URL_STREAM = "http://aasaretv.com/getURLStream.php";
+
+}
